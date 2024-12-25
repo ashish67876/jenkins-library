3a99c1b0abdc74a920c0e42ed6c0dbff9406c467
Ashish Yadav <ahish233001@gmail.com>
2024-12-25 23:46:39 +0530

    Update cloneReposiroty.groovy
    new

--- vars/cloneReposiroty.groovy
+++ vars/cloneReposiroty.groovy
@@ -1,19 +1,8 @@
-/**
- * Clones a Git repository.
- *
- * @param repoUrl The Git repository URL (required).
- * @param branch  The branch to clone (optional, defaults to 'main').
- */
-def call(String repoUrl, String branch = 'main') {
-    if (!repoUrl) {
-        error "Repository URL is required to clone the repository."
+def call(Map config) {
+    if (!config.repoUrl) {
+        error "repoUrl is required"
     }
-
-    echo "Cloning repository: ${repoUrl} on branch: ${branch}"
-
-    checkout([
-        $class: 'GitSCM',
-        branches: [[name: "*/${branch}"]],
-        userRemoteConfigs: [[url: repoUrl]]
-    ])
+    echo "Cloning repository from ${config.repoUrl}"
+    checkout([$class: 'GitSCM', branches: [[name: '*/main']],
+              userRemoteConfigs: [[url: config.repoUrl]]])
 }
