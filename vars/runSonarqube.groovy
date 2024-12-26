9a9ed8ebc464c6eb8dc53ff9131ae102d490ca99
Ashish Yadav <ahish233001@gmail.com>
2024-12-26 12:49:11 +0530

    Update runSonarqube.groovy
    update

--- vars/runSonarqube.groovy
+++ vars/runSonarqube.groovy
@@ -1,25 +1,14 @@
-def call(Map config = [:]) {
-    pipeline {
-        agent any
-        environment {
-            SONAR_PROJECT_KEY = "${config.sonarProjectKey ?: 'default-key'}"
-        }
-        stages {
-            stage('SonarQube Analysis') {
-                steps {
-                    script {
-                        withSonarQubeEnv('SonarQube') { // Replace 'SonarQube' with your server name
-                            sh """
-                            mvn sonar:sonar \
-                                -Dsonar.projectKey=${SONAR_PROJECT_KEY} \
-                                -Dsonar.host.url=${env.SONAR_HOST_URL} \
-                                -Dsonar.login=${env.SONAR_AUTH_TOKEN}
-                            """
-                        }
-                    }
-                }
-            }
+def call(String projectKey, String branchName) {
+    stage("SonarQube Analysis") {
+        withSonarQubeEnv('SonarQube') { // Replace with your SonarQube server name
+            sh """
+                sonar-scanner \
+                -Dsonar.projectKey=${projectKey} \
+                -Dsonar.branch.name=${branchName} \
+                -Dsonar.sources=. \
+                -Dsonar.host.url=${env.SONAR_HOST_URL} \
+                -Dsonar.login=${env.SONAR_AUTH_TOKEN}
+            """
         }
     }
 }
-
